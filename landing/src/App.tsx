7b5240ba6e19dd26f59e01344d9ea6cc40ef52bc
GitarisTWorkSpace <spd2020.work@gmail.com>
2024-09-01 14:14:27 +0500

    init

---
+++ landing/src/App.tsx
@@ -0,0 +1,14 @@
+import styles from './page.module.css'
+
+function App() {
+  return (
+    <div className={styles.main}>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Ты думал тут что-то будет?</h1>
+        <video src={require('./video/1681476894_looped_1681476894.mp4')} className={styles.video} controls loop autoPlay muted></video>
+      </div>
+    </div>
+  );
+}
+
+export default App;
